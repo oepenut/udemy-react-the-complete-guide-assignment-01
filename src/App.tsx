276ad746bb20e16384195dc189a7cdfd5f507d05
Oscar Penut <openut@applaudostudios.com>
2021-02-18 15:49:10 -0600

    :sparkles: update app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import UserInput from './components/UserInput';
+import UserOutput from './components/UserOutput';
+
 import './App.css';
 
 function App() {
+  const [username, setUserName] = useState('username');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newUserName = event.target.value;
+    setUserName(newUserName);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UserInput username={username} change={handleChange} />
+      <UserOutput username={username} />
     </div>
   );
 }
